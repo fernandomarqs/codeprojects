889a7c6ac546fe771ea9b70934be4ca29c863b7e
Fernando Marques <f247093@dac.unicamp.br>
2024-04-26 16:05:40 -0300

    update with error handling

--- web_scraper/main.py
+++ web_scraper/main.py
@@ -5,27 +5,33 @@ from bs4 import BeautifulSoup
 
 def get_information(response):
     # function that gets the contents from the desired URL. returns the title and body of its content as strings
-    parser = BeautifulSoup(response.content, 'html.parser')
+    if response.status_code == 200:
+        parser = BeautifulSoup(response.content, 'html.parser')
 
-    title = parser.find('h1').get_text() #in html, the title is in the div <h1>
+        title = parser.find('h1').get_text() #in html, the title is in the div <h1>
 
-    paragraphs = parser.find_all('p') #gets all the paragraphs that are on the <p> divs
+        paragraphs = parser.find_all('p') #gets all the paragraphs that are on the <p> divs
 
-    index = 0
+        index = 0
 
-    #in order to ignore disclaimer paragraphs that are not relevant to us, we check the size of the paragraphs and only start on a paragraph that is >= 100 in size
-    for i, paragraph in enumerate(paragraphs):
-        if len(paragraph.get_text()) >= 100:
-            index = i
-            break
+        #in order to ignore disclaimer paragraphs that are not relevant to us, we check the size of the paragraphs and only start on a paragraph that is >= 100 in size
+        for i, paragraph in enumerate(paragraphs):
+            if len(paragraph.get_text()) >= 100:
+                index = i
+                break
 
-    #make the paragraphs list only start at the desired index after the disclaimers
-    paragraphs = paragraphs[index:]
+        #make the paragraphs list only start at the desired index after the disclaimers
+        paragraphs = paragraphs[index:]
 
-    #join the text of all paragraphs divided by a '\n'
-    body = '\n'.join([paragraph.get_text() for paragraph in paragraphs])
+        #join the text of all paragraphs divided by a '\n'
+        body = '\n'.join([paragraph.get_text() for paragraph in paragraphs])
 
-    return title, body
+        return title, body
+    else:
+        print(f"\nFailed to retrieve the data of the page. Error code: {response.status_code}")
+
+        return None, None
+
     
 
 url = input('Input the URL you wish to get informations from:\n')
@@ -35,7 +41,9 @@ response = requests.get(url)
 
 title, body = get_information(response)
 
-print('\n' + title + '\n')
-print(body)
-
+if title and body:
+    print('\n' + title + '\n')
+    print(body)
+else:
+    print('\nCould not properly access the data of that URL. Try again!')
 
